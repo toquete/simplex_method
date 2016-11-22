874c9a793dcac45c4af5ae20a0329928a9f072ee
Webster Lima <websterhlima@gmail.com>
2016-11-22 01:25:47 -0200

    Cobrindo casos para 1 solução

--- simplex.py
+++ simplex.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 def print_matrix(matrix):
+    for xablau in range(0, len(matrix[0])):
+        print '%6d' % xablau,
+    print '\n'
     for i, row in enumerate(matrix):
+
         for col in row:
             print '%6.2f' % col,
         print ''
@@ -10,192 +14,226 @@ def print_matrix(matrix):
     print '\n'
 
 
-#Funcao
-func = [4, 1, 1]
+def simplex(func, sa):
 
-#Funcao objetivo artificial
-w = []
+    #Funcao objetivo artificial
+    w = []
 
-sa = [
-    { 'type': 'e', 'idx': [2.0, 1.0, 2.0], 'z': 4.0},
-    { 'type': 'e', 'idx': [3.0, 3.0, 1.0], 'z': 3.0},
-    # { 'type': 'e', 'idx': [1, 2], 'z': 12}
-]
+    # Base
+    b = []
 
-#Add variaveis de folga ou de excesso
-for eq in sa:
-    if eq['type'] != 'e':
-        for eq_ in sa:
-            if eq_ == eq:
-                eq_['idx'].append(1 * (1 if eq_['type'] == 'le' else -1))
-            else:
-                eq_['idx'].append(0)
+    #Add variaveis de folga ou de excesso
+    for eq in sa:
+        if eq['type'] != 'e':
+            for i, eq_ in enumerate(sa):
+                if eq_ == eq:
+                    if eq_['type'] == 'le':
+                        b.append([i + 1, len(eq_['idx'])])
+                        eq_['idx'].append(1)
+                    else:
+                        eq_['idx'].append(-1)
+                else:
+                    eq_['idx'].append(0)
 
-#Add variaveis artificiais
-for eq in sa:
-    if eq['type'] != 'le':
-        for eq_ in sa:
-            if eq_ == eq:
-                eq_['idx'].append(1)
+    #Add variaveis artificiais
+    for eq in sa:
+        if eq['type'] != 'le':
+            for i, eq_ in enumerate(sa):
+                if eq_ == eq:
+                    b.append([i + 1, len(eq_['idx'])])
 
-                #Add variavel na funcao objetivo artifical
-                w.append(1)
-            else:
-                eq_['idx'].append(0)
+                    eq_['idx'].append(1)
 
-#Montando matriz do simplex
-mat = [eq['idx'] + [eq['z']] for eq in sa]
+                    #Add variavel na funcao objetivo artifical
+                    w.append(1)
+                else:
+                    eq_['idx'].append(0)
 
-#Montando matriz do simplex da funcao objetivo artificial
-w = [0] * (len(mat[0]) - len(w) - 1) + w + [0]
-mat = [w] + mat
+    # print "ENCONTROU A BASE"
+    # print bnew
 
-#Definir quem e a base
-b = []
-for c_idx, col in enumerate(mat[0]):
-    if col == 1:
-        #Pega linha do pivo
-        for r_idx, row in enumerate(mat[1:]):
-            if row[c_idx] == 1:
-                b.append([r_idx + 1, c_idx])
-                break
+    #Montando matriz do simplex
+    mat = [eq['idx'] + [eq['z']] for eq in sa]
 
-print 'FASE 1\n'
+    #Montando matriz do simplex da funcao objetivo artificial
+    w = [0] * (len(mat[0]) - len(w) - 1) + w + [0]
+    mat = [w] + mat
 
-print_matrix(mat)
+    #Definir quem e a base
+    # b = []
+    # for c_idx, col in enumerate(mat[0]):
+    #     if col == 1:
+    #         #Pega linha do pivo
+    #         for r_idx, row in enumerate(mat[1:]):
+    #             if row[c_idx] == 1:
+    #                 b.append([r_idx + 1, c_idx])
+    #                 break
 
-steps = 0
-#Primeira fase
-while True:
-    print 'PASSO %d' % steps
-
-    for b_ in b:
-
-        #Zera as colunas da base
-        for r_idx, row in enumerate(mat):
-            if r_idx != b_[0]:
-                coefficient = -(row[b_[1]] / mat[b_[0]][b_[1]])
-
-                m_row = np.mat(row)
-                m_row_base = np.mat(mat[b_[0]])
-
-                mat[r_idx] = np.asarray(m_row + (m_row_base * coefficient)).reshape(-1).tolist()
+    print 'FASE 1\n'
 
     print_matrix(mat)
 
-    b_tmp = None
-    b_tmp_value = 0
+    steps = 0
+    #Primeira fase
+    while True:
+        print b
 
-    #Verifica se a funcao pode crescer
-    for c_idx, col in enumerate(mat[0][:-1]):
-        if col < b_tmp_value:
-            b_tmp = c_idx
-            b_tmp_value = col
+        print 'PASSO %d' % steps
 
-    if b_tmp == None:
-        break
+        for b_ in b:
 
-    #Estudo do bloqueio
-    bloq_tmp = None
-    bloq_tmp_value = 999999 #Rever essa PORRA
+            #Zera as colunas da base
+            for r_idx, row in enumerate(mat):
+                if r_idx != b_[0]:
+                    coefficient = -(row[b_[1]] / mat[b_[0]][b_[1]])
 
-    for b_ in b:
-        #Ve qual dos valores e menor
-        try:
-            bloq_aux = mat[b_[0]][-1] / mat[b_[0]][b_tmp]
-        except:
-            bloq_aux = 0
+                    m_row = np.mat(row)
+                    m_row_base = np.mat(mat[b_[0]])
 
-        if bloq_aux >= 0 and bloq_aux < bloq_tmp_value:
-            bloq_tmp_value = bloq_aux
-            bloq_tmp = b_
+                    mat[r_idx] = np.asarray(m_row + (m_row_base * coefficient)).reshape(-1).tolist()
 
-    #Altera no vetor base com a nova variavel
-    b[b.index(bloq_tmp)][1] = b_tmp
+        print_matrix(mat)
 
-    new_b = b[b.index(bloq_tmp)]
+        b_tmp = None
+        b_tmp_value = 0
 
-    #Divide a linha pro pivo ser igual a 1
-    mat[new_b[0]] = np.asarray(np.mat(mat[new_b[0]]) / mat[new_b[0]][b_tmp]).reshape(-1).tolist()
+        #Verifica se a funcao pode crescer
+        for c_idx, col in enumerate(mat[0][:-1]):
+            if col < b_tmp_value:
+                b_tmp = c_idx
+                b_tmp_value = col
 
-    steps += 1
+        if b_tmp == None:
+            break
 
-#Removendo as colunas das variaveis artificiais
-mat = [x[:-3] + x[-1:] for x in mat]
+        #Estudo do bloqueio
+        bloq_tmp = None
+        bloq_tmp_value = 999999
 
-#Montando a matriz do simplex
-f = func + [0] * (len(mat[0]) - len(func))
-mat = [f] + mat[1:]
+        for b_ in b:
+            #Ve qual dos valores e menor
+            try:
+                bloq_aux = mat[b_[0]][-1] / mat[b_[0]][b_tmp]
+            except:
+                bloq_aux = -999999
 
-print 'FASE 2\n'
-print_matrix(mat)
+            if bloq_aux >= 0 and bloq_aux <= bloq_tmp_value:
+                bloq_tmp_value = bloq_aux
+                bloq_tmp = b_
 
-steps = 0
-#Segunda fase
-while True:
-    print 'PASSO %d' % steps
+        #Altera no vetor base com a nova variavel
+        b[b.index(bloq_tmp)][1] = b_tmp
 
-    for b_ in b:
+        new_b = b[b.index(bloq_tmp)]
 
-        #Zera as colunas da base
-        for r_idx, row in enumerate(mat):
-            if r_idx != b_[0]:
-                coefficient = -(row[b_[1]] / mat[b_[0]][b_[1]])
+        #Divide a linha pro pivo ser igual a 1
+        mat[new_b[0]] = np.asarray(np.mat(mat[new_b[0]]) / mat[new_b[0]][b_tmp]).reshape(-1).tolist()
 
-                m_row = np.mat(row)
-                m_row_base = np.mat(mat[b_[0]])
+        steps += 1
 
-                mat[r_idx] = np.asarray(m_row + (m_row_base * coefficient)).reshape(-1).tolist()
+        if steps == 10:
+            break
 
+    #Removendo as colunas das variaveis artificiais
+    if not all(w_ == 0 for w_ in w):
+        mat = [x[:-3] + x[-1:] for x in mat]
+
+    #Encontrando a base
+    # b = []
+    # for c_idx, col in enumerate(mat[0]):
+    #     b_row = 0
+
+    #     found_one = 0
+    #     found_not_zero = 0
+
+    #     for r_idx, row in enumerate(mat[1:]):
+
+    #         if row[c_idx] == 1:
+    #             found_one += 1
+    #             b_row = r_idx + 1
+    #         elif row[c_idx] != 0:
+    #             found_not_zero += 1
+
+    #     if found_one == 1 and found_not_zero == 0:
+    #         b.append([b_row, c_idx])
+
+    #Montando a matriz do simplex
+    f = func + [0] * (len(mat[0]) - len(func))
+    mat = [f] + mat[1:]
+
+    print 'FASE 2\n'
     print_matrix(mat)
 
-    b_tmp = None
-    b_tmp_value = 0
+    steps = 0
+    #Segunda fase
+    while True:
+        print 'PASSO %d' % steps
 
-    #Verifica se a funcao pode crescer
-    for c_idx, col in enumerate(mat[0][:-1]):
-        if col < b_tmp_value:
-            b_tmp = c_idx
-            b_tmp_value = col
+        for b_ in b:
 
-    if b_tmp == None:
-        break
+            #Zera as colunas da base
+            for r_idx, row in enumerate(mat):
+                if r_idx != b_[0]:
+                    coefficient = -(row[b_[1]] / mat[b_[0]][b_[1]])
 
-    #Estudo do bloqueio
-    bloq_tmp = None
-    bloq_tmp_value = 999999 #Rever essa PORRA
+                    m_row = np.mat(row)
+                    m_row_base = np.mat(mat[b_[0]])
+
+                    mat[r_idx] = np.asarray(m_row + (m_row_base * coefficient)).reshape(-1).tolist()
+
+        print_matrix(mat)
+
+        b_tmp = None
+        b_tmp_value = 0
+
+        #Verifica se a funcao pode crescer
+        for c_idx, col in enumerate(mat[0][:-1]):
+            if col < b_tmp_value:
+                b_tmp = c_idx
+                b_tmp_value = col
+
+        if b_tmp == None:
+            break
+
+        #Estudo do bloqueio
+        bloq_tmp = None
+        bloq_tmp_value = 999999 #Rever essa PORRA
+
+        for b_ in b:
+            #Ve qual dos valores e menor
+            try:
+                bloq_aux = mat[b_[0]][len(mat[b_[0]]) - 1] / mat[b_[0]][b_tmp]
+            except:
+                bloq_aux = None
+
+            if bloq_aux != None and bloq_aux >= 0 and bloq_aux < bloq_tmp_value:
+                bloq_tmp_value = bloq_aux
+                bloq_tmp = b_
+
+
+        print b
+        print b_tmp
+        print bloq_tmp
+         #Altera no vetor base com a nova variavel
+        b[b.index(bloq_tmp)][1] = b_tmp
+
+        new_b = b[b.index(bloq_tmp)]
+
+        #Divide a linha pro pivo ser igual a 1
+        mat[new_b[0]] = np.asarray(np.mat(mat[new_b[0]]) / mat[new_b[0]][b_tmp]).reshape(-1).tolist()
+
+        steps += 1
+
+        if steps == 10:
+            break
+
+    result = [0] * len(func)
 
     for b_ in b:
-        #Ve qual dos valores e menor
-        try:
-            bloq_aux = mat[b_[0]][len(mat[b_[0]]) - 1] / mat[b_[0]][b_tmp]
-        except:
-            bloq_aux = 0
+        if b_[1] < len(result):
+            result[b_[1]] = mat[b_[0]][-1]
 
-        if bloq_aux >= 0 and bloq_aux < bloq_tmp_value:
-            bloq_tmp_value = bloq_aux
-            bloq_tmp = b_
+    # for i, x in enumerate(result):
+    #     print 'x%d = %.2f' % (i + 1, x)
 
-     #Altera no vetor base com a nova variavel
-    b[b.index(bloq_tmp)][1] = b_tmp
-
-    new_b = b[b.index(bloq_tmp)]
-
-    #Divide a linha pro pivo ser igual a 1
-    mat[new_b[0]] = np.asarray(np.mat(mat[new_b[0]]) / mat[new_b[0]][b_tmp]).reshape(-1).tolist()
-
-    # print_matrix(mat)
-
-    steps += 1
-
-print 'Z = %.2f' % -mat[0][-1]
-
-result = [0] * len(func)
-
-for b_ in b:
-    result[b_[1]] = mat[b_[0]][-1]
-
-for i, x in enumerate(result):
-    print 'x%d = %.2f' % (i + 1, x)
-
-# result = [f]
+    return result, -mat[0][-1]
---
+++ test_simplex.py
@@ -0,0 +1,139 @@
+import unittest
+
+from simplex import simplex
+
+
+class SimplexTest(unittest.TestCase):
+
+    # @unittest.skip("")
+    def test_one(self):
+        func = [4, 1, 1]
+
+        sa = [
+            { 'type': 'e', 'idx': [2.0, 1.0, 2.0], 'z': 4.0},
+            { 'type': 'e', 'idx': [3.0, 3.0, 1.0], 'z': 3.0}
+        ]
+
+        result, x = simplex(func, sa)
+
+        self.assertAlmostEqual(x, 2.2)
+
+        expected = [0, 0.4, 1.8]
+        
+        for i, x in enumerate(result):
+            self.assertAlmostEqual(x, expected[i])
+
+    # @unittest.skip("")
+    def test_two(self):
+        func = [-6, 1]
+
+        sa = [
+            { 'type': 'le', 'idx': [4.0, 1.0], 'z': 21.0},
+            { 'type': 'ge', 'idx': [2.0, 3.0], 'z': 13.0},
+            { 'type': 'e', 'idx': [-1.0, 1.0], 'z': 1.0},
+        ]
+
+        result, x = simplex(func, sa)
+
+        self.assertAlmostEqual(x, -19)
+
+        expected = [4, 5]
+        
+        for i, x in enumerate(result):
+            self.assertAlmostEqual(x, expected[i])
+
+    # @unittest.skip("")
+    def test_lista1(self):
+        func = [4, 6]
+
+        sa = [
+            { 'type': 'ge', 'idx': [3.0, 2.0], 'z': 24.0},
+            { 'type': 'ge', 'idx': [2.0, 6.0], 'z': 30.0}
+        ]
+
+        result, z = simplex(func, sa)
+
+        self.assertAlmostEqual(z, 42)
+
+        expected = [6, 3]
+        
+        for i, x in enumerate(result):
+            self.assertAlmostEqual(x, expected[i])
+
+    # @unittest.skip("")
+    def test_lista2(self):
+        func = [-4, -5]
+
+        sa = [
+            { 'type': 'e', 'idx': [1.0, 1.0], 'z': 98.0},
+            { 'type': 'ge', 'idx': [2.0, -1.0], 'z': 10.0},
+            { 'type': 'le', 'idx': [-2.0, 1.0], 'z': 5.0}
+        ]
+
+        result, z = simplex(func, sa)
+
+        self.assertAlmostEqual(z, -454)
+
+        expected = [36, 62]
+        
+        for i, x in enumerate(result):
+            self.assertAlmostEqual(x, expected[i])
+
+    @unittest.skip("")
+    def test_lista4(self):
+        func = [2, 2, 3]
+
+        sa = [
+            { 'type': 'ge', 'idx': [-1.0, 2.0, 5.0], 'z': 30.0},
+            { 'type': 'ge', 'idx': [3.0, 4.0, -1.0], 'z': 60.0},
+            { 'type': 'e', 'idx': [2.0, 1.0, 2.0], 'z': 40.0}
+        ]
+
+        result, z = simplex(func, sa)
+
+        self.assertAlmostEqual(z, 5)
+
+        expected = [11, 8, 5]
+        
+        for i, x in enumerate(result):
+            self.assertAlmostEqual(x, expected[i])
+
+    # @unittest.skip("")
+    def test_lista5(self):
+        func = [-10, -12]
+
+        sa = [
+            { 'type': 'le', 'idx': [1.0, 1.0], 'z': 100.0},
+            { 'type': 'le', 'idx': [2.0, 3.0], 'z': 270.0}
+        ]
+
+        result, z = simplex(func, sa)
+
+        self.assertAlmostEqual(z, -1140)
+
+        expected = [30, 70]
+        
+        for i, x in enumerate(result):
+            self.assertAlmostEqual(x, expected[i])
+
+    # @unittest.skip("")
+    def test_lista6(self):
+        func = [-2, -3, -4]
+
+        sa = [
+            { 'type': 'le', 'idx': [1.0, 1.0, 1.0], 'z': 100.0},
+            { 'type': 'le', 'idx': [2.0, 1.0, 0.0], 'z': 210.0},
+            { 'type': 'le', 'idx': [1.0, 0.0, 0.0], 'z': 80.0}
+        ]
+
+        result, z = simplex(func, sa)
+
+        self.assertAlmostEqual(z, -400)
+
+        expected = [0, 0, 100]
+        
+        for i, x in enumerate(result):
+            self.assertAlmostEqual(x, expected[i])
+
+if __name__ == '__main__':
+    unittest.main()
